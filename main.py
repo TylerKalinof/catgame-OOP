b8f1973166981191bdab2832a71c19c664992bd7
TylerKalinof <ty.bangar@laetottenham.ac.uk>
2024-11-11 12:06:05 +0000

    art

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ class Cat:
         self.energy = 5 
 
     def play(self):
-        print(f"{self.name} is playing...")
+        print(f"""{self.name} is playing..."
+               _._     _,-'""`-._
+              (,-.`._,'(       |\`-/|
+                  `-.-' \ )-`( , o o)
+                        `-    \`_`"'-
+              """)
         self.weight -= 0.01
         self.energy -= 1
 
@@ -18,7 +23,26 @@ class Cat:
         self.intelligence -= 0.1
 
     def sleep(self):
-        print(f"{self.name} is eating...")
+        print(f"""{self.name} is sleeping...
+              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣉⣙⣷⡦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣴⠖⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣉⠛⠓⠶⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣽⡿⠟⠁⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⡶⠶⠛⠉⠉⠉⠉⠁⠀⠀⠀⠀⠉⠙⠿⠓⠒⠶⢺⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢶⡶⠶⠶⠶⠖⠖⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠘⣧⡀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⠻⢤⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⢀⣿⡿⠶⠶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⡇⠀⣀⣀⠀⢀⣀⣤⡀⠀⠀⠀⠀⠸⠶⠶⠚⠛⠋⢹⣿⣿⣟⣉⠉⠒⠀⠻⣦⣠⣤⣤⣤⣄⣀⠀⠀
+⠀⢀⣤⢾⣿⣷⣶⡍⠙⠙⠛⠋⠉⠀⠀⢴⡶⠀⠀⠀⠀⢀⣠⡶⠟⠛⠛⣷⠀⠉⠁⠀⠀⠈⣧⡀⠀⠩⣀⠈⢹⣆
+⠀⣠⠔⢉⡴⢿⣿⡟⠛⠛⠛⠶⣤⣀⠀⠀⠀⠀⠀⠀⣴⡿⠋⠀⠀⠀⢀⡉⠀⠀⠀⠀⢀⣼⠛⠛⢛⣿⡿⠀⣾⡟
+⠀⠁⣰⠋⢀⡿⠁⠀⠀⠀⠀⠀⠀⠉⠻⣦⡀⠀⠀⣼⠟⠀⠀⠀⢀⣠⣾⢁⣀⣤⣴⡶⠟⣁⣴⠞⠋⠉⢀⣼⣿⠁
+⠀⠀⠉⠀⠈⠷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡗⠚⡏⠀⢀⣤⡶⠛⠋⠉⠉⠉⠉⠀⣠⣾⠟⢁⣀⣤⣶⣿⠟⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠑⠲⠤⣄⣦⣤⡴⠞⠁⠀⠉⠙⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠹⠿⠾⠾⠟⠛⠁⠀⠀⠀⠀""")
+        
         self.energy += 5
 
     def show_stats(self):
@@ -27,3 +51,47 @@ Intelligence: {self.intelligence}
 Weight: {self.weight}       
 Energy: {self.energy}         
 """)
+
+
+cats = []
+cats_dict = {}
+
+def new_cat():
+    c_name = input("Enter your cat name: ")
+    c_colour = input("Enter your cat colour: ")
+    print(f"""Hello {c_name} \n 
+          |\---/|
+          | o_o |
+           \_^_/
+          """)
+    return c_name, c_colour
+
+while True:
+    print("1. Add cat\n 2. Play\n 3. Eat\n 4. Sleep\n 5. Show stats")
+    opp = input("What would you like to do? ")
+
+    if opp == "1":
+        c_name, c_colour = new_cat()
+        my_cat = Cat(c_name, c_colour)
+        cats.append(my_cat)
+        cats_dict[my_cat.name] = my_cat 
+    elif opp == "2":
+        for cat in cats:
+            print(cat.name)
+        cat = input("which cat would you like to play with: ")
+        cats_dict[cat].play()
+    elif opp == "3":
+        for cat in cats:
+            print(cat.name)
+        cat = input("which cat would you like to play with: ")
+        cats_dict[cat].eat()
+    elif opp == "4":
+        for cat in cats:
+            print(cat.name)
+        cat = input("which cat would you like to play with: ")
+        cats_dict[cat].sleep()
+    elif opp == "5":
+        for cat in cats:
+            print(cat.name)
+        cat = input("which cat would you like to view stats of: ")
+        cats_dict[cat].show_stats()
